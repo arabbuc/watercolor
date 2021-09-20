df258b82bb4bbc2df0b86b9d6e31f90ec8f37288
arabbuc <abarmando@gmail.com>
2021-09-20 10:21:56 +0200

    Routing OK

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { PrincipalComponent } from './components/principal/principal.component';
 import { SobremiComponent } from './components/sobremi/sobremi.component';
 import { WcardComponent } from './components/wcard/wcard.component';
-
-
 const routes: Routes = [
   {path:'',component:PrincipalComponent},
   {path:'about',component:SobremiComponent},
   {path:'contact',component:WcardComponent},
 ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
+  exports: [RouterModule] })
 export class AppRoutingModule { }
